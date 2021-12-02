61ee8b7a33ced71a3794986a783df38d2c48e5b4
Michael Adam <mycppfeed@protonmail.com>
2021-12-02 23:08:54 +0530

    Improve initsource

--- InitSource.py
+++ InitSource.py
@@ -22,18 +22,34 @@ import argparse
 def ParseArgs():
     parser = argparse.ArgumentParser(prog='Initialize Source Code')
     parser.add_argument('-p', '--path', dest='path', type=Path, required = True, help='Path of new source code')
+    parser.add_argument('-r', '--root', dest='root', default = False, action='store_true', help='Treat as root path')
     return parser.parse_args()
 
-def InitPath(dir: Path, path: Path, cmakeContent: str):
-    (dir / path).mkdir(parents = True, exist_ok = False)
-    (dir / path / path.with_suffix(".cpp")).touch()
-    (dir / path / "CMakeLists.txt").write_text(cmakeContent)
-    with (dir / "CMakeLists.txt").open("a") as f:
-        f.write("\nadd_subdirectory(" + str(path) + ")")
+def getExts(dir, is_root):
+    if is_root: return []
+    if dir == Path("tests"): return ["Test.cpp"]
+    else: return [".cpp", ".hpp"]
+
+def InitPath(dir: Path, args, cmakeContent: str):
+    curName = args.path.name
+
+    (dir / args.path).mkdir(parents = True, exist_ok = False)
+
+    for ext in getExts(dir, args.root):
+        (dir / args.path / (str(curName) + ext)).touch()
+        if ".cpp" in ext: (dir / args.path / (str(curName) + ext)).write_text("#include \"" + curName + ".hpp\"")
+
+    with (dir / args.path.parent / "CMakeLists.txt").open("a") as f:
+        f.write("\nadd_subdirectory(" + str(curName) + ")")
+
+    if not args.root:
+        (dir / args.path / "CMakeLists.txt").write_text(cmakeContent)
+
 
 if __name__ == "__main__":
     args = ParseArgs()
     print("Path = {}".format(args.path))
+    print("IsRoot = {}".format(args.root))
 
-    InitPath(Path("srcs"), args.path, srcCMakeContent)
-    InitPath(Path("tests"), args.path, srcCMakeContent)
+    InitPath(Path("srcs"), args, srcCMakeContent)
+    InitPath(Path("tests"), args, testCMakeContent)
